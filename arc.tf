bac3db7cc9387cdc77f4d4b35d6fe496530ad13e
Lucas Silveira <lucassilveira@luizalabs.com>
2025-10-01 15:58:53 -0300

    Fix: Formatação arc.tf

--- arc.tf
+++ arc.tf
@@ -4,22 +4,22 @@ data "azurerm_kubernetes_cluster" "arc_cluster" {
 }
 
 resource "azurerm_k8s_flux_configuration" "app_config" {
-  name                = "soat-app-config"
-  cluster_id          = data.azurerm_kubernetes_cluster.arc_cluster.id
-  namespace           = "flux-system"
-  scope               = "cluster"
+  name       = "soat-app-config"
+  cluster_id = data.azurerm_kubernetes_cluster.arc_cluster.id
+  namespace  = "flux-system"
+  scope      = "cluster"
 
   git_repository {
-    url      = var.gitops_repo_url
-    ssh_private_key = file("~/.ssh/id_rsa") 
+    url             = var.gitops_repo_url
+    ssh_private_key = file("~/.ssh/id_rsa")
 
-    reference_type = "branch"
+    reference_type  = "branch"
     reference_value = "main"
   }
 
   kustomization {
-    name = "rails-app"
-    path = "./apps"
+    name                     = "rails-app"
+    path                     = "./apps"
     sync_interval_in_seconds = 300
   }
 }
