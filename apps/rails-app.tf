ecabfcab8a8ce5aeb748fdd7e16070eff3e9867f
Lucas Rodrigues Silveira <111925756+Silveira-R-Lucas@users.noreply.github.com>
2025-10-06 03:45:32 +0000

    correção arquivo rails-app.tf

--- apps/rails-app.tf
+++ apps/rails-app.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_deployment_v1" "rails_app" {
   metadata {
-    name = "rails-app-deployment"
+    name   = "rails-app-deployment"
     labels = {
       app = "rails-app"
     }
@@ -27,10 +27,9 @@ resource "kubernetes_deployment_v1" "rails_app" {
           name = kubernetes_secret_v1.magalu_registry_secret.metadata[0].name
         }
 
-      spec {
         container {
           name  = "rails"
-          image = "container-registry.br-se1.magalu.cloud/soat-challenge-2/soat_tech_challenge:latest"
+          image = "container-registry.br-se1.magalu.cloud/soat-challenge-2/soat_tech_challenge:${var.rails_app_image_tag}"
 
           port {
             container_port = 3000
@@ -41,6 +40,7 @@ resource "kubernetes_deployment_v1" "rails_app" {
               name = kubernetes_secret_v1.rails_app_secrets.metadata[0].name
             }
           }
+          
           env_from {
             secret_ref {
               name = kubernetes_secret_v1.rails_db_secret.metadata[0].name
