0520aa8ca8902d7ea1f6eed9a7322f7521a81d9c
Lucas Silveira <silveira.codes@gmail.com>
2025-10-06 15:41:37 -0300

    adjust variable cluster kubeconfig  output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,45 @@
 output "cluster_kubeconfig_content" {
-  description = "O conteúdo do kubeconfig para o cluster EKS, pronto para ser usado pelo kubectl."
-  value       = module.kubernetes_cluster.kubeconfig
+  description = "O conteúdo do kubeconfig para o cluster EKS, gerado manualmente."
   sensitive   = true
+  
+  value = yamlencode({
+    apiVersion      = "v1"
+    kind            = "Config"
+    current-context = module.kubernetes_cluster.cluster_name
+    clusters = [
+      {
+        name = module.kubernetes_cluster.cluster_name
+        cluster = {
+          server                   = module.kubernetes_cluster.cluster_endpoint
+          certificate-authority-data = module.kubernetes_cluster.cluster_certificate_authority_data
+        }
+      }
+    ]
+    contexts = [
+      {
+        name = module.kubernetes_cluster.cluster_name
+        context = {
+          cluster = module.kubernetes_cluster.cluster_name
+          user    = module.kubernetes_cluster.cluster_name
+        }
+      }
+    ]
+    users = [
+      {
+        name = module.kubernetes_cluster.cluster_name
+        user = {
+          exec = {
+            apiVersion = "client.authentication.k8s.io/v1beta1"
+            command    = "aws"
+            args = [
+              "eks",
+              "get-token",
+              "--cluster-name",
+              module.kubernetes_cluster.cluster_name,
+            ]
+          }
+        }
+      }
+    ]
+  })
 }
