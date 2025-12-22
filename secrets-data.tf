343b1cdfaf91eaf247f29c384453ac79f8b0193b
Lucas Silveira <silveira.codes@gmail.com>
2025-12-22 18:35:29 -0300

    adding dynamic image tags for microservices

--- secrets-data.tf
+++ secrets-data.tf
@@ -34,9 +34,58 @@ data "aws_secretsmanager_secret_version" "mongodb_url" {
   secret_id = "soat/db/mongodb_url"
 }
 
-
 locals {
   database_url_val = length(data.aws_secretsmanager_secret_version.db_url) > 0 ? data.aws_secretsmanager_secret_version.db_url[0].secret_string : ""
   redis_url_val    = length(data.aws_secretsmanager_secret_version.redis_url) > 0 ? data.aws_secretsmanager_secret_version.redis_url[0].secret_string : ""
   mongodb_url_val  = length(data.aws_secretsmanager_secret_version.mongodb_url) > 0 ? data.aws_secretsmanager_secret_version.mongodb_url[0].secret_string : ""
+}
+
+data "aws_secretsmanager_secrets" "payment_tag_search" {
+  filter {
+    name   = "name"
+    values = ["soat/payment-service/image_tag"]
+  }
+}
+
+data "aws_secretsmanager_secret_version" "payment_tag_val" {
+  count     = length(data.aws_secretsmanager_secrets.payment_tag_search.names) > 0 ? 1 : 0
+  secret_id = "soat/payment-service/image_tag"
+}
+
+locals {
+  payment_tag = data.aws_secretsmanager_secret_version.payment_tag_val.secret_string
+}
+
+
+data "aws_secretsmanager_secrets" "kitchen_tag_search" {
+  filter {
+    name   = "name"
+    values = ["soat/kitchen-service/image_tag"]
+  }
+}
+
+data "aws_secretsmanager_secret_version" "kitchen_tag_val" {
+  count     = length(data.aws_secretsmanager_secrets.kitchen_tag_search.names) > 0 ? 1 : 0
+  secret_id = "soat/kitchen-service/image_tag"
+}
+
+locals {
+  kitchen_tag = data.aws_secretsmanager_secret_version.kitchen_tag_val.secret_string
+}
+
+
+data "aws_secretsmanager_secrets" "order_tag_search" {
+  filter {
+    name   = "name"
+    values = ["soat/order-service/image_tag"]
+  }
+}
+
+data "aws_secretsmanager_secret_version" "order_tag_val" {
+  count     = length(data.aws_secretsmanager_secrets.order_tag_search.names) > 0 ? 1 : 0
+  secret_id = "soat/order-service/image_tag"
+}
+
+locals {
+  order_tag = data.aws_secretsmanager_secret_version.order_tag_val.secret_string
 }
