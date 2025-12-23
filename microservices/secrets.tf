68ad78b7cc4530579e1464e106a49f038f7cd747
Lucas Silveira <silveira.codes@gmail.com>
2025-12-22 22:43:19 -0300

    fix bug with secrets nams

--- microservices/secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_secret_v1" "rails_db_secret" {
-  metadata {
-    name = "${var.app_name}db-secret"
-    namespace = var.namespace
-  }
-  data = {
-    DATABASE_URL = var.database_url
-  }
-}
