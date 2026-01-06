5c5dd6259148aa76af2539ca26bbe92a8dcf310a
Lucas Silveira <silveira.codes@gmail.com>
2026-01-06 20:47:28 -0300

    fix error on variable master key app name consumer deployment

--- microservices/data.tf
+++ microservices/data.tf
@@ -1,5 +1,5 @@
 data "aws_secretsmanager_secret" "app_master_key" {
-  name = "soat/${var.app_name}/master_key"
+  name = "soat/${var.master_key_app_name != null ? var.master_key_app_name : var.app_name}/master_key"
 }
 
 data "aws_secretsmanager_secret_version" "app_master_key_val" {
